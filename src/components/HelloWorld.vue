ea787246387aaaaccccb439952ac380eb1d58683
zhangyuzhe7 <zhangyuzhe7@jd.com>
2022-07-05 09:28:35 +0800

    基础脚手架提交

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,26 @@
+<script setup>
+import { ref } from 'vue'
+
+
+</script>
+
+<template>
+
+  <div class="title">
+    <img class="image" src="https://naiveui.oss-cn-hongkong.aliyuncs.com/naivelogo.svg" />
+    <h1>Naive UI Template</h1>
+    <p>这是一个简单的项目模板，集成了naive-ui、vite、axios、tailwind、pinpa</p>
+  </div>
+
+</template>
+
+<style scoped>
+.title{
+  margin-top: 150px;
+  text-align: center;
+  height: 200px;
+}
+.image{
+  width:64px;
+}
+</style>
